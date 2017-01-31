a76b5d207b30bab49e7f33d91564bd80a6ce04f3
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-01-31 10:11:07 +0530

    Structure

---
+++ IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/view/CreditsActivity.java
@@ -0,0 +1,16 @@
+package com.avoid.ihaveatheory.view;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.avoid.ihaveatheory.R;
+
+public class CreditsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+
+}
