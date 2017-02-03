237e3b4aea9d3c49c617a8ebfc33579f4f82e20c
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-02-03 20:01:27 +0530

    Crafting interface - partially completed

--- IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/widget/OptionButton.java
+++ IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/widget/OptionButton.java
@@ -9,24 +9,24 @@ import android.widget.Button;
 import com.avoid.ihaveatheory.global.Font;
 import com.avoid.ihaveatheory.util.FontCache;
 
-public class ListItemButton extends Button {
-    public ListItemButton(Context context) {
+public class OptionButton extends Button {
+    public OptionButton(Context context) {
         super(context);
         applyCustomFont(context);
     }
 
-    public ListItemButton(Context context, AttributeSet attrs) {
+    public OptionButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         applyCustomFont(context);
     }
 
-    public ListItemButton(Context context, AttributeSet attrs, int defStyleAttr) {
+    public OptionButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         applyCustomFont(context);
     }
 
-    private void applyCustomFont(Context context){
-        Typeface customFont = FontCache.getTypeface(Font.ACTION_MAN_BOLD_FONT, context);
+    private void applyCustomFont(Context context) {
+        Typeface customFont = FontCache.getTypeface(Font.KOMIKAX_FONT, context);
         setTypeface(customFont);
 
         setTextColor(Color.BLACK);
