078ded2d966d66434d9a339a0ebe9434a5ec0b19
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-02-03 10:17:25 +0530

    Craft interface layout

---
+++ IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/widget/ListItemButton.java
@@ -0,0 +1,19 @@
+package com.avoid.ihaveatheory.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class ListItemButton extends Button {
+    public ListItemButton(Context context) {
+        super(context);
+    }
+
+    public ListItemButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ListItemButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
