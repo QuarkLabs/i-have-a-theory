bd3e1bfa0cf92d1cb122151254fe820eb4f9e344
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-02-06 18:45:43 +0530

    Add background music service

---
+++ IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/global/BackgroundMusic.java
@@ -0,0 +1,61 @@
+package com.avoid.ihaveatheory.global;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.avoid.ihaveatheory.R;
+
+import java.io.IOException;
+
+/**
+ * Created by sumedhe on 2/6/17.
+ */
+
+public class BackgroundMusic extends Service{
+    MediaPlayer player;
+    public IBinder onBind(Intent arg0) {
+
+        return null;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player = MediaPlayer.create(this, R.raw.app_background_music);
+        player.setLooping(true); // Set looping
+        player.setVolume(100,100);
+
+    }
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player.start();
+        return 1;
+    }
+
+    public void onStart(Intent intent, int startId) {
+        // TO DO
+    }
+    public IBinder onUnBind(Intent arg0) {
+        // TO DO Auto-generated method
+        return null;
+    }
+
+    public void onStop() {
+
+    }
+    public void onPause() {
+
+    }
+    @Override
+    public void onDestroy() {
+        player.stop();
+        player.release();
+    }
+
+    @Override
+    public void onLowMemory() {
+
+    }
+}
