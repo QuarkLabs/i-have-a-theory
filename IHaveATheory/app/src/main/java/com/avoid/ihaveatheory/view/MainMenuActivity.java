02a843c3d081a00a0a8f841c3d82f301c161072b
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-02-07 02:28:30 +0530

    Config button click sound

--- IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/view/MainMenuActivity.java
+++ IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/view/MainMenuActivity.java
@@ -1,6 +1,7 @@
 package com.avoid.ihaveatheory.view;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,6 +15,8 @@ import com.avoid.ihaveatheory.widget.MainButton;
 
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    static MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,21 +36,36 @@ public class MainMenuActivity extends AppCompatActivity {
         Intent svc=new Intent(this, BackgroundMusic.class);
         startService(svc);
 
+        //Button Click Sound
+        mp = MediaPlayer.create(this, R.raw.app_button_click);
     }
 
     public void onClickContinueButton(View view) {
         Session.saveFileHandler.loadGame();
-
+        playClickSound();
         startActivity(new Intent(MainMenuActivity.this, DashboardActivity.class));
     }
 
     public void onClickNewGameButton(View view) {
         Session.saveFileHandler.newGame();
-
+        playClickSound();
         startActivity(new Intent(MainMenuActivity.this, ScenarioActivity.class));
     }
 
     public void onClickOptionsButton(View view) {
+        playClickSound();
         Toast.makeText(getApplicationContext(), "This feature is not yet implemented.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (this.isFinishing()){
+            stopService(new Intent(this, BackgroundMusic.class));
+        }
+    }
+
+    public static void playClickSound(){
+        mp.start();
     }
 }
