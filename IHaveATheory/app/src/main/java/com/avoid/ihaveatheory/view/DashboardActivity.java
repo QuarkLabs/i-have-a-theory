a12cae7dd23ab51661f179a3542171c1f20d9844
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-02-12 10:37:48 +0530

    Modify and Configure dashboard buttons

--- IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/view/DashboardActivity.java
+++ IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/view/DashboardActivity.java
@@ -9,7 +9,6 @@ import android.widget.RelativeLayout;
 import com.avoid.ihaveatheory.R;
 import com.avoid.ihaveatheory.global.Session;
 import com.avoid.ihaveatheory.model.PlaybleActivity;
-import com.avoid.ihaveatheory.widget.HexButton;
 import com.avoid.ihaveatheory.widget.LabelTextView;
 import com.avoid.ihaveatheory.widget.LinearProgressBar;
 
@@ -114,32 +113,7 @@ public class DashboardActivity extends AppCompatActivity implements PlaybleActiv
 
     @Override
     public void setIcons() {
-        HexButton gatherButton = (HexButton) findViewById(R.id.gather_button);
-        gatherButton.setIcon(R.drawable.dashboard_gather);
 
-        HexButton craftButton = (HexButton) findViewById(R.id.craft_button);
-        craftButton.setIcon(R.drawable.dashboard_craft);
-
-        HexButton backpackButton = (HexButton) findViewById(R.id.backpack_button);
-        backpackButton.setIcon(R.drawable.dashboard_backpack);
-
-        HexButton shopButton = (HexButton) findViewById(R.id.shop_button);
-        shopButton.setIcon(R.drawable.dashboard_shop);
-
-        HexButton mapButton = (HexButton) findViewById(R.id.map_button);
-        mapButton.setIcon(R.drawable.dashboard_map);
-
-        HexButton exploreButton = (HexButton) findViewById(R.id.explore_button);
-        exploreButton.setIcon(R.drawable.dashboard_explore);
-
-        HexButton cookingPotButton = (HexButton) findViewById(R.id.cooking_pot_button);
-        cookingPotButton.setIcon(R.drawable.dashboard_cooking_pot);
-
-        HexButton shelterButton = (HexButton) findViewById(R.id.shelter_button);
-        shelterButton.setIcon(R.drawable.dashboard_shelter);
-
-        HexButton journalButton = (HexButton) findViewById(R.id.journal_button);
-        journalButton.setIcon(R.drawable.dashboard_journal);
     }
 
     private void refreshStats() {
