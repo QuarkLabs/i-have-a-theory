656ae6f9be823700bc1a4abd2fe316c8af98e878
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-02-14 12:18:03 +0530

    Choice activity complete. (without the API)

--- IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/view/ChoiceActivity.java
+++ IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/view/ChoiceActivity.java
@@ -3,6 +3,8 @@ package com.avoid.ihaveatheory.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.avoid.ihaveatheory.R;
+
 public class ChoiceActivity extends AppCompatActivity {
 
     @Override
