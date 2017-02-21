c4c51fac20f88f3469646abbdb83e2096888946b
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-02-21 09:48:35 +0530

    Modify Story Activity

--- IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/util/Common.java
+++ IHaveATheory/app/src/main/java/com/avoid/ihaveatheory/util/Common.java
@@ -6,12 +6,20 @@ import android.util.Log;
 
 import com.avoid.ihaveatheory.game.choice.Choice;
 import com.avoid.ihaveatheory.view.ProgressActivity;
+import com.avoid.ihaveatheory.view.StoryActivity;
 
 public class Common {
-    public static void showProgressBar(Context context, String title, String time){
+    public static void showProgressBar(Context context, String title, String time) {
         Intent intent = new Intent(context, ProgressActivity.class);
         intent.putExtra("title", title);
         intent.putExtra("time", time);
+        context.startActivity(intent);
+    }
+
+    public static void showStory(Context context, String message, String ok_text) {
+        Intent intent = new Intent(context, StoryActivity.class);
+        intent.putExtra("title", message);
+        intent.putExtra("time", ok_text);
         context.startActivity(intent);
     }
 
